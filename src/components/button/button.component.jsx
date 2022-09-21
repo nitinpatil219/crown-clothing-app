857e61bd4975db1a5657cb0efb45ec629b4ffe22
Nitin Patil <nitinpatil219@gmail.com>
2022-09-21 09:49:27 +0530

    created sign up component ,the form input component, button component

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,24 @@
+/* 
+default-button
+
+inverted-button
+
+google Sign In-button
+*/
+import "./button.style.scss";
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
